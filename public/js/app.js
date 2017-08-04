ec00b9dc177f51d8ab94d4e9b4808b760ddefc5a
Dmitry Ryumkin <riumkin@gmail.com>
2017-08-04 20:02:08 +0300

    Refactoring: change binding this by using arrow functions

--- public/js/app.js
+++ public/js/app.js
@@ -1,42 +1,62 @@
+/* eslint-disable no-param-reassign, operator-assignment */
 
 class ProductList extends React.Component {
-  handleProductUpVote(productId) {
-    console.log(productId + ' was updated')
+
+  state = {
+    products: [],
   }
+
+  componentDidMount() {
+    this.setState({ products: Seed.products });
+  }
+
+  // Inside `ProductList`
+  handleProductUpVote = (productId) => {
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, {
+          votes: product.votes + 1,
+        });
+      } else {
+        return product;
+      }
+    });
+    this.setState({
+      products: nextProducts,
+    });
+  }
+
   render() {
-    const products = Seed.products.sort((a, b) => (
+    const products = this.state.products.sort((a, b) => (
       b.votes - a.votes
     ));
-    const productComponents = Seed.products.map((product) => (
-        <Product 
-          key={'product-' + product.id}
-          id={product.id}
-          title={product.title}  
-          description={product.description}
-          url={product.url}
-          votes={product.votes}
-          submitterAvatarUrl={product.submitterAvatarUrl}
-          productImageUrl={product.productImageUrl}
-          onVote={this.handleProductUpVote}
-        />
+    const productComponents = products.map((product) => (
+      <Product
+        key={'product-' + product.id}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+        onVote={this.handleProductUpVote}
+      />
     ));
     return (
       <div className='ui unstackable items'>
         {productComponents}
-    </div> );
+      </div>
+    );
   }
 }
 
 class Product extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.handleUpVote = this.handleUpVote.bind(this);
+  handleUpVote = () => {
+    this.props.onVote(this.props.id);
   }
-  
-  handleUpVote() {
-    this.props.onVote(this.props.id)
-  }
+
   render() {
     return (
       <div className='item'>
@@ -63,11 +83,14 @@ class Product extends React.Component {
             <img
               className='ui avatar image'
               src={this.props.submitterAvatarUrl}
-            /> </div>
+            />
           </div>
+        </div>
       </div>
-); }
+    );
+  }
 }
+
 ReactDOM.render(
   <ProductList />,
   document.getElementById('content')
